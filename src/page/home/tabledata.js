e35812e4455be80ce07cf4bbd2a7bda6f3d8e80f
wesal-nowsher <wesalnama111@gmail.com>
2021-12-30 01:35:17 +0500

    axios used and data fetched and used

--- src/page/home/tabledata.js
+++ src/page/home/tabledata.js
@@ -1,18 +1,21 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import icons from '../../assets/icons.png';
+import loaderimg from '../../assets/loader.png';
 
  function Tabledata(){
     
-    
+    const [jobData, setJobData] = useState([]);
     useEffect(() => {
       axios.get("https://api.kuratedao.xyz/set/12").then((res)=>{
-        // console.log("res:",res)
-        console.log("res:",JSON.parse(res && res.data))
-        if(res && res.data){
-            let parsedata=JSON.parse();
-            console.log("wesa", parsedata);
-        }
+        console.log("res:",res)
+        let newdata=[];
+        console.log("res:",res.data)
+        res.data.map((one)=>{
+            console.log("res:", JSON.parse(one[0]))
+            newdata.push(JSON.parse(one[0]));
+        })
+        setJobData([...newdata]);
 
       })
       },[]);
@@ -20,202 +23,50 @@ import icons from '../../assets/icons.png';
         <>
             <div className="row">
                 <div className="tabledata-padding">
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
+                    {
+                         jobData && jobData.length===0 &&
+                         <div className="d-flex justify-content-center">
+                             <img  src={loaderimg}/>
+                         </div> 
+                    }
+                 {
+                    jobData && jobData.length>0 && jobData.map((item)=>{
+                        return(
+                            <div className="row 4">
+                            <div className="col-5 d-flex">
+                                <div className="logo">
+                                    <img src={icons} />
+                                </div>
+                                <div className="d-flex flex-column desifg">
+                                    <h4>{item.title}</h4>
+                                    <p>{item.company}</p>
+                                </div>
                             </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
+                            <div className="col-5 d-flex ">
+                                <div className="location">
+                                    <p>{item.description}</p>
+                                </div>
+                                <div className="tags-buttons-selected">
+                                    <button className="btn">Non-Tech</button>
+                                    <button className="btn">Sales</button>
+                                    <button className="btn">Marketing</button>
+                                    <button className="btn">Communicty Manager</button>
+        
+                                </div>
+        
+                            </div>
+                            <div className="col-2 d-flex justify-content-end align-items-center">
+                                <div className="location"><p>6y</p></div>
+                                <div className="apply-btn"><button className="btn">Apply</button></div>
                             </div>
                         </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
+                        )
+                    })
+                   
 
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
-                    <div className="row 4">
-                        <div className="col-5 d-flex">
-                            <div className="logo">
-                                <img src={icons} />
-                            </div>
-                            <div className="d-flex flex-column desifg">
-                                <h4>Front-End Developer</h4>
-                                <p>Parcel</p>
-                            </div>
-                        </div>
-                        <div className="col-5 d-flex ">
-                            <div className="location">
-                                <p>Remote, NYC</p>
-                            </div>
-                            <div className="tags-buttons-selected">
-                                <button className="btn">Non-Tech</button>
-                                <button className="btn">Sales</button>
-                                <button className="btn">Marketing</button>
-                                <button className="btn">Communicty Manager</button>
-
-                            </div>
-
-                        </div>
-                        <div className="col-2 d-flex justify-content-end align-items-center">
-                            <div className="location"><p>6y</p></div>
-                            <div className="apply-btn"><button className="btn">Apply</button></div>
-                        </div>
-                    </div>
+                }
+                   
+                 
                    
                 </div>
               
